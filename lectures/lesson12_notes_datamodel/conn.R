173ca573f71ebb20bce660209040ce9d41b5487f
DarioBoh <bonarettidario@gmail.com>
2019-02-25 16:29:23 -0600

    lesson 12 update

--- lectures/lesson12_notes_datamodel/conn.R
+++ lectures/lesson12_notes_datamodel/conn.R
@@ -1,5 +1,3 @@
-library(tidyverse)
-if (!require(DBI)) {install.packages('DBI'); library(DBI)}
 con <- DBI::dbConnect(RMySQL::MySQL(), 
                       host = "ba-isdsclass-dev2.lsu.edu",
                       user = "student",
@@ -7,5 +5,5 @@ con <- DBI::dbConnect(RMySQL::MySQL(),
                       dbname = "hotelreviews")
 
 #test whether you can query the db 
-print('If you get an error, try reinstalling `openssl``, restart R and rerun the script.')
+print('If you get an error, try reinstalling `openssl`, restart R and rerun the script.')
 
